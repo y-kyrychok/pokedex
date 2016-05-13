9e53e757d803bdc5f5c8476f5d15fe1998e88792
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-05-13 19:21:30 +0300

    implement getNextPokemons helper

--- index.js
+++ index.js
@@ -2,10 +2,31 @@
 {
     "use strict"
 
-    const api = "http://pokeapi.co/api/v1"
+    const api = "http://pokeapi.co"
 
-    let request = new XMLHttpRequest
-        request.responseType = "json"
-        request.open("GET", `${api}/pokemon?limit=12`)
-        request.send()
+    let getNextPokemons = (() =>
+    {
+        let next = "/api/v1/pokemon?limit=12"
+
+        return callback =>
+        {
+            let request = new XMLHttpRequest
+                request.responseType = "json"
+                request.open("GET", `${api}${next}`)
+                request.send()
+
+            request.onload = () =>
+            {
+                let
+                {
+                    meta: {next},
+                    objects
+                }
+                = request.response
+
+                callback(objects)
+            }
+        }
+    })()
+
 })()
